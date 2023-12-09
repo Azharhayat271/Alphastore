2177bebf99745cfa9b90a8df75be7b63eca04282
Azharhayat271 <115901212+Azharhayat271@users.noreply.github.com>
2023-12-09 13:35:03 +0500

    issues fix

--- web_admin/src/components/Sidebar.js
+++ web_admin/src/components/Sidebar.js
@@ -1,50 +1,69 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+//fuction to handle handleClick
+const handleClick = () => {
+  localStorage.removeItem('userInfo');
+  window.location.href = '/login';
+}
 
 const Sidebar = () => {
-	return(
-			<>
-			<nav className="sidebar sidebar-offcanvas" id="sidebar">
-            <ul className="nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  <i className="fa fa-home menu-icon" aria-hidden="true"></i>
-                  <span className="menu-title">Dashboard</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/products">
-                  <i className="fa fa-cube menu-icon" />
-                  <span className="menu-title">Products</span>
-                </Link>
-              </li>
-              {/* <li className="nav-item">
+  return (
+    <>
+      <nav className="sidebar sidebar-offcanvas" id="sidebar">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              <i className="fa fa-home menu-icon" aria-hidden="true"></i>
+              <span className="menu-title">Dashboard</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/products">
+              <i className="fa fa-cube menu-icon" />
+              <span className="menu-title">Products</span>
+            </Link>
+          </li>
+          {/* <li className="nav-item">
                 <Link className="nav-link" to="/categories">
                   <i className="fa fa-list menu-icon" />
                   <span className="menu-title">Categories</span>
                 </Link>
               </li> */}
-              <li className="nav-item">
-                <Link className="nav-link" to="/orders">
-                  <i className="fa fa-shopping-cart menu-icon" />
-                  <span className="menu-title">Orders</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users">
-                  <i className="fa fa-user menu-icon" />
-                  <span className="menu-title">Users</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/storesetting">
-                  <i className="fa fa-cogs menu-icon" />
-                  <span className="menu-title">Store Setting</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-			</>
-		)
+          <li className="nav-item">
+            <Link className="nav-link" to="/orders">
+              <i className="fa fa-shopping-cart menu-icon" />
+              <span className="menu-title">Orders</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/users">
+              <i className="fa fa-user menu-icon" />
+              <span className="menu-title">Users</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/storesetting">
+              <i className="fa fa-cogs menu-icon" />
+              <span className="menu-title">Store Setting</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/marketing">
+              <i className="fa fa-rectangle-ad menu-icon" />
+              <span className="menu-title">Marketing</span>
+            </Link>
+          </li>
+          <li className="nav-item" onClick={handleClick}>
+            <Link className="nav-link">
+
+              <i className="fa fa-sign-out menu-icon" onClick={handleClick} />
+              <span className="menu-title">Logout</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
 
 
--- web_admin/src/components/pages/Dashboard.js
+++ web_admin/src/components/pages/Dashboard.js
@@ -198,33 +198,33 @@ const Dashboard = () => {
                           <div className="col-sm-12">
                             <div className="statistics-details d-flex align-items-center justify-content-between">
                               <div>
-                                <p className="statistics-title">Bounce Rate</p>
-                                <h3 className="rate-percentage">32.53%</h3>
-                                <p className="text-danger d-flex"><i className="mdi mdi-menu-down" /><span>-0.5%</span></p>
+                                <p className="statistics-title">Total Products</p>
+                                <h3 className="rate-percentage">4</h3>
+                                <p className="text-success d-flex"><i className="mdi mdi-menu-up" /><span>-0.5%</span></p>
                               </div>
                               <div>
-                                <p className="statistics-title">Page Views</p>
-                                <h3 className="rate-percentage">7,682</h3>
-                                <p className="text-success d-flex"><i className="mdi mdi-menu-up" /><span>+0.1%</span></p>
+                                <p className="statistics-title">Total Orders</p>
+                                <h3 className="rate-percentage">10</h3>
+                                <p className="text-success d-flex"><i className="mdi mdi-menu-down" /><span>+0.1%</span></p>
                               </div>
                               <div>
-                                <p className="statistics-title">New Sessions</p>
-                                <h3 className="rate-percentage">68.8</h3>
+                                <p className="statistics-title">Total Sale</p>
+                                <h3 className="rate-percentage">2253</h3>
                                 <p className="text-danger d-flex"><i className="mdi mdi-menu-down" /><span>68.8</span></p>
                               </div>
                               <div className="d-none d-md-block">
-                                <p className="statistics-title">Avg. Time on Site</p>
-                                <h3 className="rate-percentage">2m:35s</h3>
+                                <p className="statistics-title">Order Shipped</p>
+                                <h3 className="rate-percentage">8</h3>
                                 <p className="text-success d-flex"><i className="mdi mdi-menu-down" /><span>+0.8%</span></p>
                               </div>
                               <div className="d-none d-md-block">
-                                <p className="statistics-title">New Sessions</p>
-                                <h3 className="rate-percentage">68.8</h3>
+                                <p className="statistics-title">Total Users</p>
+                                <h3 className="rate-percentage">1</h3>
                                 <p className="text-danger d-flex"><i className="mdi mdi-menu-down" /><span>68.8</span></p>
                               </div>
                               <div className="d-none d-md-block">
-                                <p className="statistics-title">Avg. Time on Site</p>
-                                <h3 className="rate-percentage">2m:35s</h3>
+                                <p className="statistics-title">Orders Pending</p>
+                                <h3 className="rate-percentage">3</h3>
                                 <p className="text-success d-flex"><i className="mdi mdi-menu-down" /><span>+0.8%</span></p>
                               </div>
                             </div>
